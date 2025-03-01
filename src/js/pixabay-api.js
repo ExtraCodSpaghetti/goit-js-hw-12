2c850e44e615ebefa58f43595ca8eda87cd8358f
Kostiantyn Chesak <K03.chesak@gmail.com>
2025-03-01 15:44:11 +0100

    fix

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+const keyAPI = '49114219-148fd2eac33b6a5671d248709';
+
+export const fetchPhotos = (query, currentPage) => {
+  const axiosOptions = {
+    params: {
+      key: keyAPI,
+      q: query,
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: true,
+      per_page: 15,
+      page: currentPage,
+    },
+  };
+
+  return axios.get('https://pixabay.com/api/', axiosOptions);
+};
